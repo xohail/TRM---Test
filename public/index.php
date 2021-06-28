08cd37ba8d1cbbd36f983f81d561602460d279e6
Sohail <sohail@Sohail-Sentrals-MacBook-Pro.local>
2021-06-28 10:18:59 +1000

    Initial push to setup Docker based PHP/PHP-Unit env

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use TheRMTest\App;
+
+$app = new App();
+
+$app->sayHello();
---
+++ src/App.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TheRMTest;
+
+
+class App
+{
+    public function sayHello(): void
+    {
+        echo 'Hello!';
+    }
+}
