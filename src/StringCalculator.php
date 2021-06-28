aa3207bfdc3e3c70c443dff30a017e9d2fb846b7
Sohail Rahman <engr.sohail36@gmail.com>
2021-06-28 13:12:33 +1000

    Added tests for empty, single digit and 2 digit string

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace TheRMTest;
 
-
 class StringCalculator
 {
     public function intAdd(): void
--- tests/MyTest.php
+++ tests/MyTest.php
@@ -7,12 +7,44 @@ class MyTest extends TestCase
 {
     protected $string_calculator;
 
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->string_calculator = new StringCalculator();
+    }
+
     /**
      * Test if the StringCalculator class exists
      */
     public function testSetup(): void
     {
-        $this->string_calculator = new StringCalculator();
         $this->assertTrue($this->string_calculator instanceof StringCalculator);
+    }
+
+    /**
+     * Test returns 0 if empty string is provided
+     */
+    public function testEmptyString(): void
+    {
+        $sum = $this->string_calculator->intAdd("");
+        $this->assertSame(0, $sum);
+    }
+
+    /**
+     * Test returns same digit passed in string
+     */
+    public function testSingleDigitString(): void
+    {
+        $sum = $this->string_calculator->intAdd("1");
+        $this->assertEquals(1, $sum);
+    }
+
+    /**
+     * Test returns sum of the 2 digits passed in string
+     */
+    public function testTwoDigitString(): void
+    {
+        $sum = $this->string_calculator->intAdd("1,2");
+        $this->assertEquals(3, $sum);
     }
 }
